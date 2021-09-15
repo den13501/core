ecc6f49bc5159b65f9733765cb0f8b2224a42d4a
-Shiken- <58270741+den13501@users.noreply.github.com>
2021-09-16 02:16:08 +0800

    Add Unending Breath to warlock bots

--- src/game/PlayerBots/CombatBotBaseAI.cpp
+++ src/game/PlayerBots/CombatBotBaseAI.cpp
@@ -1180,6 +1180,12 @@ void CombatBotBaseAI::PopulateSpellData()
                         m_spells.warlock.pLifeTap->Id < pSpellEntry->Id)
                         m_spells.warlock.pLifeTap = pSpellEntry;
                 }
+				else if (pSpellEntry->SpellName[0].find("Unending Breath") != std::string::npos)
+				{
+					if (!m_spells.warlock.pUnendingBreath ||
+						m_spells.warlock.pUnendingBreath->Id < pSpellEntry->Id)
+						m_spells.warlock.pUnendingBreath = pSpellEntry;
+				}
                 break;
             }
             case CLASS_WARRIOR:
