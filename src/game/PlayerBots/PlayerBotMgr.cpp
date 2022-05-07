7921dc06f01be8838e3c24798c774fd34b3734e4
-Shiken- <den13501@yahoo.com.tw>
2022-05-07 17:26:24 +0800

    Prevent setting a custom level for bots unless the account is a GM or skipping checks is enabled. #1491
    召喚partybot時，限制僅admin權限帳號才可利用第二參數指定等級，普通帳號的第二參數則是性別(0/1)

--- src/game/PlayerBots/PlayerBotMgr.cpp
+++ src/game/PlayerBots/PlayerBotMgr.cpp
@@ -883,7 +883,12 @@ bool ChatHandler::HandlePartyBotAddCommand(char* args)
 		}
 
         //ExtractUInt32(&args, botLevel); //prevent player add level argument禁止使用第二個參數=botlevel
-		ExtractUInt32(&args, botGender); //使用第二個參數=botGender
+
+		// Prevent setting a custom level for bots unless the account is a GM or skipping checks is enabled.
+		if (GetSession()->GetSecurity() > SEC_PLAYER || sWorld.getConfig(CONFIG_BOOL_PARTY_BOT_SKIP_CHECKS))
+			ExtractUInt32(&args, botLevel);
+		else
+			ExtractUInt32(&args, botGender); //使用第二個參數=botGender
     }
 
 	if (!botClass) //命令沒有輸入職業的話
