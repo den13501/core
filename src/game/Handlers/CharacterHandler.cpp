01edb3d0622c580ea620e4b30e52bb3c47a2e425
-Shiken- <den13501@yahoo.com.tw>
2022-05-14 02:35:05 +0800

    Initial Solocraft commit
    Credit to Niam5, acidmanifesto and the TrinityCore project for the heavy legwork on getting this created
    Refer to https://github.com/Niam5/mangos-classic/commit/e78cb05afd7843278973fbe7ff89dbb9b301f5b4

--- src/game/Handlers/CharacterHandler.cpp
+++ src/game/Handlers/CharacterHandler.cpp
@@ -729,7 +729,21 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder *holder)
 
     m_playerLoading = false;
     m_clientMoverGuid = pCurrChar->GetObjectGuid();
-    delete holder;
+
+	//Start Solocraft Functions 單人單刷模式
+	bool SoloCraftEnable = sWorld.getConfig(CONFIG_BOOL_SOLOCRAFT_ENABLED);
+	bool SoloCraftAnnounceModule = sWorld.getConfig(CONFIG_BOOL_SOLOCRAFT_ANNOUNCE);
+
+	if (SoloCraftEnable)
+	{
+		if (SoloCraftAnnounceModule)
+		{
+			ChatHandler(pCurrChar->GetSession()).SendSysMessage("This server is running |cff4CFF00SPP SoloCraft Custom |rmodule.");
+		}
+	}
+	//End Solocraft Functions
+	
+	delete holder;
     if (alreadyOnline)
     {
         pCurrChar->UpdateControl();
--- src/game/Objects/Player.h
+++ src/game/Objects/Player.h
@@ -1493,7 +1493,8 @@ class Player final: public Unit
     private:
         PlayerSpellMap m_spells;
         float m_auraBaseMod[BASEMOD_END][MOD_END];
-        SpellModList m_spellMods[MAX_SPELLMOD];
+		uint16 m_baseSpellPower; //SOLOCRAFT MOD
+		SpellModList m_spellMods[MAX_SPELLMOD];
         uint32 m_lastFromClientCastedSpellID;
         
         bool IsNeedCastPassiveLikeSpellAtLearn(SpellEntry const* spellInfo) const;
@@ -1618,7 +1619,7 @@ class Player final: public Unit
         float GetMeleeCritFromAgility() const;
         float GetDodgeFromAgility() const;
         float GetSpellCritFromIntellect() const;
-        void InitStatBuffMods()
+		void InitStatBuffMods()
         {
             for (int i = STAT_STRENGTH; i < MAX_STATS; ++i) SetFloatValue(PLAYER_FIELD_POSSTAT0 + i, 0);
             for (int i = STAT_STRENGTH; i < MAX_STATS; ++i) SetFloatValue(PLAYER_FIELD_NEGSTAT0 + i, 0);
@@ -1654,7 +1655,9 @@ class Player final: public Unit
         void UpdateManaRegen() override;
         void UpdateAttackPowerAndDamage(bool ranged = false) override;
         void UpdateDamagePhysical(WeaponAttackType attType) override;
-        void UpdateSpellDamageAndHealingBonus();
+		void ApplySpellPowerBonus(int32 amount, bool apply); //SOLOCRAFT MOD
+		uint32 GetBaseSpellPowerBonus() const { return m_baseSpellPower; } //SOLOCRAFT MOD
+		void UpdateSpellDamageAndHealingBonus();
         void UpdateDefenseBonusesMod();
         void UpdateBlockPercentage();
         void UpdateCritPercentage(WeaponAttackType attType);
