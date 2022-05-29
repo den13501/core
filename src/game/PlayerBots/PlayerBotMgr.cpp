f86653bdb0f34876514eeec5611997358e3d7060
-Shiken- <den13501@yahoo.com.tw>
2022-05-30 04:08:27 +0800

    ComeToMe command minimum distance check
    cherry pick from https://github.com/fradunz/core/commit/87162ce3fc6fc1ea7ab57ffaab5d41bf8fa8fd31

--- src/game/PlayerBots/PlayerBotMgr.cpp
+++ src/game/PlayerBots/PlayerBotMgr.cpp
@@ -1446,7 +1446,7 @@ bool ChatHandler::HandlePartyBotClearMarksCommand(char* args)
 //機器人回來命令助手function
 bool HandlePartyBotComeToMeHelper(Player* pBot, Player* pPlayer)
 {
-    if (pBot->AI() && pBot->IsAlive() && pBot->IsInMap(pPlayer) && !pBot->HasUnitState(UNIT_STAT_NO_FREE_MOVE))
+    if (pBot->AI() && pBot->IsAlive() && pBot->IsInMap(pPlayer) && !pBot->HasUnitState(UNIT_STAT_NO_FREE_MOVE) && !pBot->IsWithinDistInMap(pPlayer, 10.0f))
     {
         if (PartyBotAI* pAI = dynamic_cast<PartyBotAI*>(pBot->AI()))
         {
