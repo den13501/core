a90148550b261f72403aa7439f71bf78b70f25f2
-Shiken- <den13501@yahoo.com.tw>
2022-06-03 20:37:06 +0800

    [feat]AH BOT Customize
    All credit to @fradunz
    Cherry pick from
    https://github.com/fradunz/core/commit/2e58abf51627dee934b4590cbc76f88e27cffa07
    https://github.com/fradunz/core/commit/90eb2e9f45879f4c013f645f02b49cce51262ecb
    https://github.com/fradunz/core/commit/73fbdf3e852b4c93c6cf2308869520470047d778
    ```
    - AH Bot now uses random factors for stack ammount and bid/buyout values.
    - AH Bot: Single items and the most expensive appear at half the rate of materials and consumables.
    - AH Bot Prices now rounded to a more real pricing look
    - Command to create auction in-game: Creates an auction for item ID and calculates its bid value based on sales price and quality
    ```

--- src/game/AuctionHouse/AuctionHouseBotMgr.cpp
+++ src/game/AuctionHouse/AuctionHouseBotMgr.cpp
@@ -112,6 +112,10 @@ void AuctionHouseBotMgr::Update(bool force /* = false */)
     while (auctions < items)
     {
         AuctionHouseBotEntry item = m_items[urand(0, entriesCount - 1)];
+        // Makes single items and the most expensive appear at half the rate
+        // of materials and consumables
+        if ((item.stack == 1 || item.buyout >= 1000000) && urand(0, 1))
+            continue;
         AddItem(item, auctionHouse);
         auctions++;
     }
@@ -120,6 +124,11 @@ void AuctionHouseBotMgr::Update(bool force /* = false */)
 void AuctionHouseBotMgr::AddItem(AuctionHouseBotEntry e, AuctionHouseObject *auctionHouse)
 {
     ASSERT(m_auctionHouseEntry);
+
+    uint32 itemStack = 1;
+    float priceFactor = 1.0f;
+    uint32 itemBid = 100;
+    uint32 itemBuyOut = 1000;
 
     ItemPrototype const* prototype = sObjectMgr.GetItemPrototype(e.item);
     if (prototype == nullptr)
@@ -141,23 +150,69 @@ void AuctionHouseBotMgr::AddItem(AuctionHouseBotEntry e, AuctionHouseObject *auc
     if (randomPropertyId != 0)
         item->SetItemRandomProperties(randomPropertyId);
 
-    uint32 etime = urand(1, 3);
+    uint32 etime = urand(1, 5);
     switch (etime)
     {
         case 1:
-            etime = 43200;
+            etime = urand(7200, 28800);
             break;
         case 2:
-            etime = 86400;
-            break;
         case 3:
-            etime = 172800;
+            etime = urand(28800, 57600);
+            break;
+        case 4:
+        case 5:
+            etime = 86400;
             break;
         default:
             etime = 86400;
             break;
     }
     item->SetCount(e.stack);
+
+    if (e.stack > 1)
+    {
+        switch (urand(1, 5))
+        {
+        case 1:
+            itemStack = urand(1, e.stack);
+            break;
+        case 2:
+            itemStack = 1;
+            break;
+        case 3:
+            itemStack = e.stack / 2;
+            break;
+        case 4:
+        case 5:
+            itemStack = e.stack;
+            break;
+        default:
+            itemStack = 1;
+            break;
+        }
+        priceFactor = (float)itemStack / (float)e.stack;
+    }
+    else
+        itemStack = e.stack;
+
+    if (e.bid == 0)
+        itemBid = prototype->SellPrice * 5 * itemStack;
+    else
+        itemBid = e.bid;
+    itemBid = itemBid * priceFactor * (urand(10, 30) * 0.05f);
+
+    if (e.buyout == 0)
+        itemBuyOut = itemBid * 2;
+    else
+        itemBuyOut = e.buyout;
+    itemBuyOut = itemBuyOut * priceFactor * (urand(16, 30) * 0.05f);
+    if (!urand(0, 4))
+        itemBuyOut--;
+    if (itemBid > itemBuyOut)
+        itemBid = itemBuyOut - 1;
+
+    item->SetCount(itemStack);
 
     uint32 dep = sAuctionMgr.GetAuctionDeposit(m_auctionHouseEntry, etime, item);
 
@@ -167,8 +222,8 @@ void AuctionHouseBotMgr::AddItem(AuctionHouseBotEntry e, AuctionHouseObject *auc
     auctionEntry->itemGuidLow        = item->GetGUIDLow();
     auctionEntry->itemTemplate       = item->GetEntry();
     auctionEntry->owner              = 0;
-    auctionEntry->startbid           = e.bid;
-    auctionEntry->buyout             = e.buyout;
+    auctionEntry->startbid           = itemBid;
+    auctionEntry->buyout             = itemBuyOut;
     auctionEntry->bidder             = 0;
     auctionEntry->bid                = 0;
     auctionEntry->deposit            = dep;
@@ -182,6 +237,29 @@ void AuctionHouseBotMgr::AddItem(AuctionHouseBotEntry e, AuctionHouseObject *auc
     auctionEntry->SaveToDB();
 }
 
+void AuctionHouseBotMgr::Create(uint32 itemId)
+{
+
+    ItemPrototype const* prototype = sObjectMgr.GetItemPrototype(itemId);
+    if (prototype == nullptr)
+    {
+        sLog.outInfo("AHBot::Create() : Item %u does not exist.", itemId);
+        return;
+	}
+
+    uint32 stack = prototype->GetMaxStackSize();
+    uint32 multi = prototype->Quality + 1;
+    uint32 bid = prototype->SellPrice * multi * 5 * stack;
+    uint32 buyout = bid * 2;
+
+    if (!WorldDatabase.PExecute("INSERT INTO `auctionhousebot` (`item`, `stack`, `bid`, `buyout`) VALUES (%u, %u, %u, %u)", itemId, stack, bid, buyout))
+    {
+        sLog.outInfo("AHBot::Create() : Error creating auction for item %u.", itemId);
+        return;
+    }
+
+}
+
 bool ChatHandler::HandleAHBotUpdateCommand(char *args)
 {
     sAuctionHouseBotMgr.Update(true);
@@ -193,5 +271,17 @@ bool ChatHandler::HandleAHBotReloadCommand(char *args)
 {
     sAuctionHouseBotMgr.Load();
     SendSysMessage("[AHBot] Reload finished.");
+    return true;
+}
+
+bool ChatHandler::HandleAHBotCreateCommand(char *args) //支援以指令建立拍賣
+{
+    ///- Get the command line arguments
+    uint32 itemId;
+    if (!ExtractUInt32(&args, itemId))
+        return false;
+
+    sAuctionHouseBotMgr.Create(itemId);
+    SendSysMessage("[AHBot] Auction Created.");
     return true;
 }
