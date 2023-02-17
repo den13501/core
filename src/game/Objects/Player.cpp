efae8d24bf80f505e70a8c6b6d1f0b382a7c8271
-Shiken- <den13501@yahoo.com.tw>
2023-02-18 02:12:44 +0800

    [Custom] Give pet experience regardless of player level
    ref to https://github.com/vmangos/core/commit/3dea7691e5ebb8d8fefc0490ae5a93f8c916106a

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -19996,12 +19996,17 @@ void Player::RewardSinglePlayerAtKill(Unit* pVictim)
         RewardReputation(pVictim, 1);
         GiveXP(xp, pVictim);
 
+/*
         // Pet should only gain XP if mob is not grey to Owner.
         if (xp)
         {
             if (Pet* pet = GetPet())
                 pet->GivePetXP(MaNGOS::XP::Gain(pet, static_cast<Creature*>(pVictim)));
         }
+*/
+
+        if (Pet* pet = GetPet())
+            pet->GivePetXP(MaNGOS::XP::Gain(pet, static_cast<Creature*>(pVictim))); //For fun, always give pet xp.
 
         // normal creature (not pet/etc) can be only in !PvP case
         if (pVictim->GetTypeId() == TYPEID_UNIT)
